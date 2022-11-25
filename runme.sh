79276841423597a6a5526fa0a5fc0492b0533ad9
BartTho <61376948+BartTho@users.noreply.github.com>
2022-11-25 11:29:07 +0100

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,5 @@
+wget https://github.com/BartTho/Rocky-Linux/raw/master/bestanden.zip
+wget https://github.com/BartTho/Rocky-Linux/raw/master/video.mp4
+unzip bestanden.zip
+rm bestanden.zip
+rm runme.sh
